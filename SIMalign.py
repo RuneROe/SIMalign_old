6bb560579cb50f5d0417cb2a9320a33787da5472
Rune Rahbek Østergaard <s183479@dtu.dk>
2023-11-06 17:23:56 +0100

    fix

--- SIMalign.py
+++ SIMalign.py
@@ -159,7 +159,7 @@ def SIMalign(ref_structure, structure_list_entire, iterations, tresshold_aa, max
             ref_kd = model_kd[model]
 
             # LOOP through ca atoms to find scores of one model
-            for k, ref_atom in enumerate(model.atom):
+            for ref_atom in model.atom:
                 tmp = [(structure_list_entire[i], ref_atom.index)]
                 s = 0
                 ref_resn = ref_atom.resn
@@ -211,7 +211,7 @@ def SIMalign(ref_structure, structure_list_entire, iterations, tresshold_aa, max
     if break_flag == False:
         print(f"Completed {iterations} iteration(s) of superexposion.")
     for i, structure in enumerate(structure_list_entire):
-        cmd.select(f"{structure}_core",f"{structure} and not HETATM and chain A{selection[i]})
+        cmd.select(f"{structure}_core",f"{structure} and not HETATM and chain A{selection[i]}")
     return score_list
 
 def run(ref_structure, files, iterations, tresshold_aa, max_dist, outfilename):
