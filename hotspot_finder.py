e3941cde07af52c6c36b2c18818d329324cb7727
Rune Rahbek Østergaard <s183479@dtu.dk>
2024-03-13 10:11:29 +0100

    update

--- hotspot_finder.py
+++ hotspot_finder.py
@@ -141,7 +141,7 @@ def add_hotspot(closeAA_list,align,atoms_list,resn,resi,coord,count):
                     # print(j,ref_index,count)
                     # close_index = resi_to_index(closeAA,structure_list[j],structure_list,align)
                     # if close_index != None:
-                    # print(j, closeAA)
+                    print(j, closeAA)
                     if bigger_AA(seq[closeAA],align[0][closeAA]):
                         flag = False     
                 if flag:
